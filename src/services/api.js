abed115ac3e09ce09ec98bbe5c1e8f186b92127a
hamza ziouane <42297606+ziouanes@users.noreply.github.com>
2025-04-23 00:43:47 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://prism-topaz-muscari.glitch.me/api';
 
 const apiClient = axios.create({
   baseURL: API_URL,
